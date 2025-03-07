9f101c4c78c9b81fec00d94b8dbdb5d70ff164d0
Adrian Alamu <alamujunior@gmail.com>
2025-03-07 19:32:02 +1200

    dsa

---
+++ src/main/java/group7/enrollmentSystem/models/Student.java
@@ -0,0 +1,22 @@
+package group7.enrollmentSystem.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    
+}

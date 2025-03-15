0011f9756e315ebdf0faad7ebdaadf0d84919f27
TJP08 <s11212749@student.usp.ac.fj>
2025-03-16 10:10:29 +1300

    Create ProgrammeDto.java

---
+++ src/main/java/group7/enrollmentSystem/dtos/ProgrammeDto.java
@@ -0,0 +1,10 @@
+package group7.enrollmentSystem.dtos;
+
+import lombok.Data;
+
+@Data
+public class ProgrammeDto {
+    private String programmeCode;
+    private String name;
+    private String faculty;
+}

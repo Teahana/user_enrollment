3d52cd6dc3119b5f4c112e4fd2ff94145b328156
TJP08 <s11212749@student.usp.ac.fj>
2025-03-16 09:48:26 +1300

    Create ProgrammeService.java

---
+++ src/main/java/group7/enrollmentSystem/services/ProgrammeService.java
@@ -0,0 +1,58 @@
+package group7.enrollmentSystem.services;
+
+import group7.enrollmentSystem.models.Programme;
+import group7.enrollmentSystem.repos.ProgrammeRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProgrammeService {
+
+    private final ProgrammeRepo programmeRepo;
+
+    // Create a new programme
+    public void saveProgramme(String name, String programmeCode, String faculty) {
+        Programme programme = new Programme();
+        programme.setName(name);
+        programme.setProgrammeCode(programmeCode);
+        programme.setFaculty(faculty);
+        programmeRepo.save(programme);
+    }
+
+    // Get all programmes records
+    public List<Programme> getAllProgrammes() {
+        return programmeRepo.findAll();
+    }
+
+    // Get 1 record
+    public Optional<Programme> getProgrammeByCode(String programmeCode) {
+        return programmeRepo.findByProgrammeCode(programmeCode);
+    }
+
+    // Update a programme record
+    public void updateProgramme(String programmeCode, String name, String faculty) {
+        Optional<Programme> optionalProgramme = programmeRepo.findByProgrammeCode(programmeCode);
+        if (optionalProgramme.isPresent()) {
+            Programme programme = optionalProgramme.get();
+            programme.setName(name);
+            programme.setFaculty(faculty);
+            programmeRepo.save(programme);
+        } else {
+            throw new RuntimeException("Programme not found with programmecode: " + programmeCode);
+        }
+    }
+
+    // Delete a programme record
+    public void deleteProgramme(String programmeCode) {
+        Optional<Programme> optionalProgramme = programmeRepo.findByProgrammeCode(programmeCode);
+        if (optionalProgramme.isPresent()) {
+            programmeRepo.delete(optionalProgramme.get());
+        } else {
+            throw new RuntimeException("Programme not found with code: " + programmeCode);
+        }
+    }
+}

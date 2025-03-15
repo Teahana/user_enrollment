40eed45d8d70de47e94528a161318d2f7df44e87
TJP08 <s11212749@student.usp.ac.fj>
2025-03-16 09:23:42 +1300

    Update ProgrammeRepo.java

--- src/main/java/group7/enrollmentSystem/repos/ProgrammeRepo.java
+++ src/main/java/group7/enrollmentSystem/repos/ProgrammeRepo.java
@@ -1,6 +1,6 @@
 package group7.enrollmentSystem.repos;
 
-import group7.enrollmentSystem.models.courses.Programme;
+import group7.enrollmentSystem.models.Programme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

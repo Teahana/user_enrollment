7690e5a9732688d07ee6480010c38cd9c6769854
TJP08 <s11212749@student.usp.ac.fj>
2025-03-16 09:58:26 +1300

    Delete src/main/java/group7/CoursePrerequisiteRepo.java

--- src/main/java/group7/CoursePrerequisiteRepo.java
+++
@@ -1,7 +0,0 @@
-package group7.enrollmentSystem.repos;
-
-import group7.enrollmentSystem.models.CoursePrerequisite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoursePrerequisiteRepo extends JpaRepository<CoursePrerequisite, Long> {
-}

f6be32b742858a4fa67b974722d6e2f57bfe2b59
Adrian Alamu <alamujunior@gmail.com>
2025-03-20 06:03:10 +1200

    Update CourseEnrollmentRepo.java

--- src/main/java/group7/enrollmentSystem/repos/CourseEnrollmentRepo.java
+++ src/main/java/group7/enrollmentSystem/repos/CourseEnrollmentRepo.java
@@ -3,6 +3,8 @@ package group7.enrollmentSystem.repos;
 import group7.enrollmentSystem.models.CourseEnrollment;
 import group7.enrollmentSystem.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,10 @@ import java.util.List;
 public interface CourseEnrollmentRepo extends JpaRepository<CourseEnrollment, Long> {
     List<CourseEnrollment> findByStudentAndCurrentlyTakingTrue(Student student);
     List<CourseEnrollment> findByStudentAndCurrentlyTakingFalse(Student student);
-    List<CourseEnrollment> findByStudent(Student student);}
+    List<CourseEnrollment> findByStudent(Student student);
+    
+    @Query("SELECT ce FROM CourseEnrollment ce WHERE ce.student.id = :studentId")
+    List<CourseEnrollment> exampleForGettingByLongId(@Param("studentId") Long id);
+}
+
 

fce94e8bd58dbaaa547b073bfe9912b9888ce6de
Adrian Alamu <alamujunior@gmail.com>
2025-03-20 11:41:40 +1200

    gg

--- src/main/java/group7/enrollmentSystem/controllers/ApiController.java
+++ src/main/java/group7/enrollmentSystem/controllers/ApiController.java
@@ -37,21 +37,21 @@ public class ApiController {
     private final CourseEnrollmentRepo courseEnrollmentRepo;
     private final StudentRepo studentRepo;
 
-    @PostMapping("/test")
-    public ResponseEntity<?> test(@RequestBody HashMap<String, Long> body) {
-        List<CourseEnrollment> data1 = courseEnrollmentRepo.findByStudentIdAndCurrentlyTakingFalse(body.get("studentId"));
-        List<CourseEnrollment> data2 = courseEnrollmentRepo.findByStudentIdAndCurrentlyTakingTrue(body.get("studentId"));
-        Student student = studentRepo.findById(body.get("studentId")).orElse(null);
-        List<CourseEnrollment> data3 = courseEnrollmentRepo.findByStudentAndCurrentlyTakingTrue(student);
-        List<CourseEnrollment> data4 = courseEnrollmentRepo.findByStudentAndCurrentlyTakingFalse(student);
-        return ResponseEntity.ok().body(new HashMap<>() {{
-            put("data1", data1);
-            put("data2", data2);
-            put("data3", data3);
-            put("data4", data4);
-        }});
-
-    }
+//    @PostMapping("/test")
+//    public ResponseEntity<?> test(@RequestBody HashMap<String, Long> body) {
+//        List<CourseEnrollment> data1 = courseEnrollmentRepo.findByStudentIdAndCurrentlyTakingFalse(body.get("studentId"));
+//        List<CourseEnrollment> data2 = courseEnrollmentRepo.findByStudentIdAndCurrentlyTakingTrue(body.get("studentId"));
+//        Student student = studentRepo.findById(body.get("studentId")).orElse(null);
+//        List<CourseEnrollment> data3 = courseEnrollmentRepo.findByStudentAndCurrentlyTakingTrue(student);
+//        List<CourseEnrollment> data4 = courseEnrollmentRepo.findByStudentAndCurrentlyTakingFalse(student);
+//        return ResponseEntity.ok().body(new HashMap<>() {{
+//            put("data1", data1);
+//            put("data2", data2);
+//            put("data3", data3);
+//            put("data4", data4);
+//        }});
+//
+//    }
     /*@PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody HashMap<String, String> data) {
         String email = data.get("email");

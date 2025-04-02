d3c8dfe10df73a6aff863d98e6e14d9d8fb548ef
Adrian Alamu <alamujunior@gmail.com>
2025-04-02 13:21:05 +1200

    final3

--- node-mermaid-svg-service/start-node.sh
+++ node-mermaid-svg-service/start-node.sh
@@ -2,13 +2,13 @@
 
 echo "✅ Running start-node.sh"
 
-# Use absolute path to Node
+#Use absolute path to Node
 NODE_PATH="/home/teahana/.nvm/versions/node/v16.20.2/bin/node"
 
-# Navigate to this script’s directory
+#Navigate to this script’s directory
 cd "$(dirname "$0")" || exit 1
 
 echo "🗂 Current directory: $(pwd)"
 
-# Start the Node server
+#Start the Node server
 "$NODE_PATH" server.js

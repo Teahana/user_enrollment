e69ae2d78e7959d537b971a3a978872da6de1476
Adrian Alamu <alamujunior@gmail.com>
2025-04-02 13:10:27 +1200

    final2

--- node-mermaid-svg-service/start-node.sh
+++ node-mermaid-svg-service/start-node.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# Make sure to use LF (not CRLF) if editing on Windows
 
 echo "✅ Running start-node.sh"
 
-# Load NVM and use Node 
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-nvm use 16
+# Use absolute path to Node
+NODE_PATH="/home/teahana/.nvm/versions/node/v16.20.2/bin/node"
 
 # Navigate to this script’s directory
 cd "$(dirname "$0")" || exit 1
 
-# Print working directory for debug
 echo "🗂 Current directory: $(pwd)"
 
-# Run the Node server
-node server.js
+# Start the Node server
+"$NODE_PATH" server.js

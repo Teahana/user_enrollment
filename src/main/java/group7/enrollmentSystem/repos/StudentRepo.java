98bf6be8fb3dd7a1b784bc344e381a10d28e601a
Aukusitino Potoi <s11212749@student.usp.ac.fj>
2025-05-29 07:02:24 +1300

    Update StudentRepo.java

--- src/main/java/group7/enrollmentSystem/repos/StudentRepo.java
+++ src/main/java/group7/enrollmentSystem/repos/StudentRepo.java
@@ -3,7 +3,6 @@ package group7.enrollmentSystem.repos;
 import group7.enrollmentSystem.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 import java.util.Optional;
 
